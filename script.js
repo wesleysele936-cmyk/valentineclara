e2cec217c174b13a49490ec0b66ccda3b72d8de7
wesleysele936-cmyk <wesleysele936@gmail.com>
2026-02-01 19:00:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-const text = document.getElementById("text");
-const buttons = document.getElementById("buttons");
-
-let noCount = 0;
-
-function handleYes() {
-  text.textContent = "YES works 💖 (next step coming)";
-}
-
-function handleNo() {
-  noCount++;
-
-  const messages = [
-    "Oops that’s the wrong answer.",
-    "Be serious chipchip.",
-    "Mfana you’ll be your own Valentine 😒",
-    "Awe you can stop now."
-  ];
-
-  if (noCount <= 4) {
-    text.textContent = messages[noCount - 1];
-  } else {
-    buttons.innerHTML = `
-      <button class="yes" onclick="handleYes()">Yes</button>
-      <button class="no" onclick="handleYes()">Obviously</button>
-    `;
-  }
-}
-
-
-}
+alert("script loaded");
 
 
 
