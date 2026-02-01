30978c310e124ff1bad0857c9244ee79fa39847a
wesleysele936-cmyk <wesleysele936@gmail.com>
2026-02-01 18:41:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const buttons = document.getElementById("buttons");
 let noCount = 0;
 
 function handleYes() {
-  text.innerHTML = "YES works 💖";
+  text.textContent = "YES works 💖";
 }
 
 function handleNo() {
   noCount++;
 
   const messages = [
-    "Clara 💖💘<br>Oops that’s the wrong answer.",
-    "Clara 💖💘<br>Be serious chipchip.",
-    "Clara 💖💘<br>Mfana you’ll be your own Valentine 😒",
-    "Clara 💖💘<br>Awe you can stop now."
+    "Oops that’s the wrong answer.",
+    "Be serious chipchip.",
+    "Mfana you’ll be your own Valentine 😒",
+    "Awe you can stop now."
   ];
 
   if (noCount <= 4) {
-    text.innerHTML = messages[noCount - 1];
+    text.textContent = messages[noCount - 1];
   } else {
     buttons.innerHTML = `
       <button class="yes" onclick="handleYes()">Yes</button>
