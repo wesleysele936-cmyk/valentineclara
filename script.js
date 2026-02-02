481b91198d2417a6af69ca251a4ea7d06e1cf41f
wesleysele936-cmyk <wesleysele936@gmail.com>
2026-02-02 22:03:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-alert("script loaded");
+const text = document.getElementById("text");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+  text.textContent = "YES BUTTON WORKS 💖";
+});
+
+noBtn.addEventListener("click", () => {
+  text.textContent = "NO BUTTON WORKS 😅";
+});
+
 
 
 
